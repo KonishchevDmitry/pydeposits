0e38b5add3fda2f130e53ffee643402b980c2035
Dmitry Konishchev <konishchev@gmail.com>
2016-02-23 17:52:04 +0300

    Update package requirements

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
 from setuptools import find_packages, setup
+from setuptools.command.test import test as Test
+
+
+class PyTest(Test):
+    def finalize_options(self):
+        Test.finalize_options(self)
+        self.test_args = ["tests"]
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        pytest.main(self.test_args)
+
 
 with open("README") as readme:
     setup(
@@ -10,7 +23,7 @@ with open("README") as readme:
         long_description=readme.read().strip(),
         url="https://github.com/KonishchevDmitry/pydeposits",
 
-        install_requires=["pcli >= 0.2", "xlrd"],
+        install_requires=["pcli >= 0.2", "requests", "xlrd"],
 
         author="Dmitry Konishchev",
         author_email="konishchev@gmail.com",
@@ -19,4 +32,7 @@ with open("README") as readme:
         entry_points={
             "console_scripts": ["pydeposits = pydeposits.main:main"],
         },
+
+        cmdclass={"test": PyTest},
+        tests_require=["pytest"],
     )
